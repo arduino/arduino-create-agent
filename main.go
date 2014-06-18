0ee828fdc3a65518c475dead455d33841cbb8ac5
John Lauer <jlauer@zipwhip.com>
2014-06-17 22:06:58 -0700

    Removed double newline from send command.
    If you added a newline to the send, the server would add a 2nd newline.
    Now it's up to the sender to choose a newline. This required refactoring
    of all commands. Also added help commands when connect to websocket.
    Also inlined the home.html file so no more dependency errors.
    
    
    Former-commit-id: e634c683f8e8f1d6d0cab9d2cdd7111f311f44c8 [formerly 506a90befec4e2d0c240c20e57b6aa3c255e737d]
    Former-commit-id: a547b00d3e44a08b83b407b5cf0ea98d6d1d52fa

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
-// Supports Windows, Linux, Mac, and Raspberry Pi
+// Version 1.2
+// Supports Windows, Linux, Mac, and Raspberry Pi, Beagle Bone Black
 
 package main
 
@@ -7,15 +8,15 @@ import (
 	"go/build"
 	"log"
 	"net/http"
-	"path/filepath"
+	//"path/filepath"
 	"text/template"
 )
 
 var (
-	VERSION   = "1.1"
-	addr      = flag.String("addr", ":8989", "http service address")
-	assets    = flag.String("assets", defaultAssetPath(), "path to assets")
-	homeTempl *template.Template
+	VERSION = "1.1"
+	addr    = flag.String("addr", ":8989", "http service address")
+	assets  = flag.String("assets", defaultAssetPath(), "path to assets")
+	//homeTempl *template.Template
 )
 
 func defaultAssetPath() string {
@@ -28,7 +29,7 @@ func defaultAssetPath() string {
 }
 
 func homeHandler(c http.ResponseWriter, req *http.Request) {
-	homeTempl.Execute(c, req.Host)
+	homeTemplate.Execute(c, req.Host)
 }
 
 func main() {
@@ -40,7 +41,7 @@ func main() {
 	flag.Parse()
 	f := flag.Lookup("addr")
 	log.Print("Started server and websocket on localhost" + f.Value.String())
-	homeTempl = template.Must(template.ParseFiles(filepath.Join(*assets, "home.html")))
+	//homeTempl = template.Must(template.ParseFiles(filepath.Join(*assets, "home.html")))
 
 	// launch the hub routine which is the singleton for the websocket server
 	go h.run()
@@ -55,3 +56,101 @@ func main() {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
+
+var homeTemplate = template.Must(template.New("home").Parse(homeTemplateHtml))
+
+// If you navigate to this server's homepage, you'll get this HTML
+// so you can directly interact with the serial port server
+const homeTemplateHtml = `<!DOCTYPE html>
+<html>
+<head>
+<title>Serial Port Example</title>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<script type="text/javascript">
+    $(function() {
+
+    var conn;
+    var msg = $("#msg");
+    var log = $("#log");
+
+    function appendLog(msg) {
+        var d = log[0]
+        var doScroll = d.scrollTop == d.scrollHeight - d.clientHeight;
+        msg.appendTo(log)
+        if (doScroll) {
+            d.scrollTop = d.scrollHeight - d.clientHeight;
+        }
+    }
+
+    $("#form").submit(function() {
+        if (!conn) {
+            return false;
+        }
+        if (!msg.val()) {
+            return false;
+        }
+        conn.send(msg.val() + "\n");
+        msg.val("");
+        return false
+    });
+
+    if (window["WebSocket"]) {
+        conn = new WebSocket("ws://{{$}}/ws");
+        conn.onclose = function(evt) {
+            appendLog($("<div><b>Connection closed.</b></div>"))
+        }
+        conn.onmessage = function(evt) {
+            appendLog($("<div/>").text(evt.data))
+        }
+    } else {
+        appendLog($("<div><b>Your browser does not support WebSockets.</b></div>"))
+    }
+    });
+</script>
+<style type="text/css">
+html {
+    overflow: hidden;
+}
+
+body {
+    overflow: hidden;
+    padding: 0;
+    margin: 0;
+    width: 100%;
+    height: 100%;
+    background: gray;
+}
+
+#log {
+    background: white;
+    margin: 0;
+    padding: 0.5em 0.5em 0.5em 0.5em;
+    position: absolute;
+    top: 0.5em;
+    left: 0.5em;
+    right: 0.5em;
+    bottom: 3em;
+    overflow: auto;
+}
+
+#form {
+    padding: 0 0.5em 0 0.5em;
+    margin: 0;
+    position: absolute;
+    bottom: 1em;
+    left: 0px;
+    width: 100%;
+    overflow: hidden;
+}
+
+</style>
+</head>
+<body>
+<div id="log"></div>
+<form id="form">
+    <input type="submit" value="Send" />
+    <input type="text" id="msg" size="64"/>
+</form>
+</body>
+</html>
+`
