e0fbdad1ff832495125f96b6314d7a126b681dc0
Martino Facchin <m.facchin@arduino.cc>
2015-04-22 10:13:25 +0200

    add compile script for webide bridge
    Former-commit-id: f1a9ee8432371c8ee6515b609f2ac03043b21c30 [formerly f7f99e4baa6b512e9e05d8feef2feb96d59da8a9]
    Former-commit-id: bbaba64c5af7d11a68fed53faffbc60ff28b31f3

---
+++ compile_webidebridge.sh
@@ -0,0 +1,19 @@
+# git submodule init
+# git submodule update
+
+cp -r arduino/tools_linux_64  arduino/tools
+goxc -os="linux" -arch="amd64" --include="arduino/hardware,arduino/tools" -n="Arduino_WebIDE_Bridge" -d=.
+rm -rf arduino/tools
+
+cp -r arduino/tools_linux_32  arduino/tools
+goxc -os="linux" -arch="386" --include="arduino/hardware,arduino/tools" -n="Arduino_WebIDE_Bridge" -d=.
+rm -rf arduino/tools
+
+cp -r arduino/tools_windows  arduino/tools
+goxc -os="windows" --include="arduino/hardware,arduino/tools" -n="Arduino_WebIDE_Bridge" -d=.
+rm -rf arduino/tools
+
+cp -r arduino/tools_darwin  arduino/tools
+goxc -os="darwin" --include="arduino/hardware,arduino/tools" -n="Arduino_WebIDE_Bridge" -d=.
+rm -rf arduino/tools
+
