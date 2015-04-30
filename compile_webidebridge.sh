f98f858cd3c69c9f34d8719a2162b2e19a1650c6
Martino Facchin <m.facchin@arduino.cc>
2015-04-30 12:04:29 +0200

    add resources to compile script
    Former-commit-id: 552f0f5623f64005123b0288cdab9c2514102f70 [formerly e9ad653184934d5c9ffdb1f7966b9fe6d4d89be7]
    Former-commit-id: 5a4ce55890d182c782e29d163171afc2a0ccabe7

--- compile_webidebridge.sh
+++ compile_webidebridge.sh
@@ -4,25 +4,25 @@
 if [ `uname` == Linux ]
 then
 cp -r arduino/tools_linux_64  arduino/tools
-CGO_ENABLED=1 goxc -os="linux" -arch="amd64" --include="arduino/hardware,arduino/tools" -n="Arduino_WebIDE_Bridge" -d=.
+CGO_ENABLED=1 goxc -os="linux" -arch="amd64" --include="arduino/hardware,arduino/tools,arduino/resources" -n="Arduino_WebIDE_Bridge" -d=.
 rm -rf arduino/tools
-
+exit 0
 cp -r arduino/tools_linux_32  arduino/tools
-CGO_ENABLED=1 goxc -os="linux" -arch="386" --include="arduino/hardware,arduino/tools" -n="Arduino_WebIDE_Bridge" -d=.
+CGO_ENABLED=1 goxc -os="linux" -arch="386" --include="arduino/hardware,arduino/tools,arduino/resources" -n="Arduino_WebIDE_Bridge" -d=.
 rm -rf arduino/tools
 
 cp -r arduino/tools_linux_arm  arduino/tools
-CGO_ENABLED=1 goxc -os="linux" -arch="arm" --include="arduino/hardware,arduino/tools" -n="Arduino_WebIDE_Bridge" -d=.
+CGO_ENABLED=1 goxc -os="linux" -arch="arm" --include="arduino/hardware,arduino/tools,arduino/resources" -n="Arduino_WebIDE_Bridge" -d=.
 rm -rf arduino/tools
 
 cp -r arduino/tools_windows  arduino/tools
-goxc -os="windows" -arch="386" --include="arduino/hardware,arduino/tools" -n="Arduino_WebIDE_Bridge" -d=. -build-ldflags="-H=windowsgui"
+goxc -os="windows" -arch="386" --include="arduino/hardware,arduino/tools,arduino/resources" -n="Arduino_WebIDE_Bridge" -d=. -build-ldflags="-H=windowsgui"
 rm -rf arduino/tools
 fi
 
 if [ `uname` == Darwin ]
 then
 cp -r arduino/tools_darwin  arduino/tools
-CGO_ENABLED=1 goxc -os="darwin" -arch="amd64" --include="arduino/hardware,arduino/tools" -n="Arduino_WebIDE_Bridge" -d=.
+CGO_ENABLED=1 goxc -os="darwin" -arch="amd64" --include="arduino/hardware,arduino/tools,arduino/resources" -n="Arduino_WebIDE_Bridge" -d=.
 rm -rf arduino/tools
 fi
