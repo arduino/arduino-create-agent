4d8c7093f09f98eda2c7ce867a27d773c01a7a6a
Matteo Suppo <matteo.suppo@gmail.com>
2015-07-02 16:28:32 +0200

    Use socket.io. Breaking change!!
    Former-commit-id: eba24734df435ec5578326bde2b4b949d52a5ca7 [formerly a026571b9c36693fb8e3030ecaa0de2b1ff44286]
    Former-commit-id: e4037954574dae5500cee8a2d4cb1bfa624f1611

--- conn.go
+++ conn.go
@@ -3,39 +3,27 @@
 package main
 
 import (
-	"github.com/gorilla/websocket"
+	"github.com/googollee/go-socket.io"
 	"log"
 	"net/http"
 )
 
 type connection struct {
 	// The websocket connection.
-	ws *websocket.Conn
+	ws socketio.Socket
 
 	// Buffered channel of outbound messages.
-	send chan []byte
-}
-
-func (c *connection) reader() {
-	for {
-		_, message, err := c.ws.ReadMessage()
-		if err != nil {
-			break
-		}
-
-		h.broadcast <- message
-	}
-	c.ws.Close()
+	send     chan []byte
+	incoming chan []byte
 }
 
 func (c *connection) writer() {
 	for message := range c.send {
-		err := c.ws.WriteMessage(websocket.TextMessage, message)
+		err := c.ws.Emit("message", string(message))
 		if err != nil {
 			break
 		}
 	}
-	c.ws.Close()
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
@@ -67,20 +55,26 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func wsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	log.Print("Started a new websocket handler")
-	ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
-	if _, ok := err.(websocket.HandshakeError); ok {
-		http.Error(w, "Not a websocket handshake", 400)
-		return
-	} else if err != nil {
-		return
+func wsHandler() *socketio.Server {
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
 	}
-	//c := &connection{send: make(chan []byte, 256), ws: ws}
-	c := &connection{send: make(chan []byte, 256*10), ws: ws}
-	h.register <- c
-	defer func() { h.unregister <- c }()
-	go c.writer()
-	c.reader()
+
+	server.On("connection", func(so socketio.Socket) {
+		c := &connection{send: make(chan []byte, 256*10), ws: so}
+		h.register <- c
+		so.On("command", func(message string) {
+			h.broadcast <- []byte(message)
+		})
+		so.On("disconnection", func() {
+			h.unregister <- c
+		})
+		go c.writer()
+	})
+	server.On("error", func(so socketio.Socket, err error) {
+		log.Println("error:", err)
+	})
+
+	return server
 }
