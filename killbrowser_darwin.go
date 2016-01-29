cfe32ca02d93b464ac0194dc7a4dfb1bb2c141f0
Matteo Suppo <matteo.suppo@gmail.com>
2016-01-29 09:41:07 +0100

    Stubs for darwin browserkiller

---
+++ killbrowser_darwin.go
@@ -0,0 +1,13 @@
+package main
+
+func findBrowser(process string) ([]byte, error) {
+	return nil, nil
+}
+
+func killBrowser(process string) ([]byte, error) {
+	return nil, nil
+}
+
+func startBrowser(command []byte, url string) ([]byte, error) {
+	return nil, nil
+}
