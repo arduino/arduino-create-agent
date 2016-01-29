58f468d7fe96cb8a2b5bbda16b960b7fae31dc7a
Matteo Suppo <matteo.suppo@gmail.com>
2016-01-29 16:34:20 +0100

    Revert "Debug informations"
    This reverts commit 797caa84f90420a4aaa055c6f1eb651621db2e80.

--- killbrowser_linux.go
+++ killbrowser_linux.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"os/exec"
 	"strings"
 )
@@ -11,33 +10,16 @@ func findBrowser(process string) ([]byte, error) {
 	grep := exec.Command("grep", process)
 	head := exec.Command("head", "-n", "1")
 
-	log.Println("ps command:")
-	log.Printf("%+v", ps)
-
-	log.Println("grep command:")
-	log.Printf("%+v", grep)
-
-	log.Println("head command:")
-	log.Printf("%+v", head)
-
 	return pipe_commands(ps, grep, head)
 }
 
 func killBrowser(process string) ([]byte, error) {
 	cmd := exec.Command("pkill", "-9", process)
-
-	log.Println("kill command:")
-	log.Printf("%+v", cmd)
-
 	return cmd.Output()
 }
 
 func startBrowser(command []byte, url string) ([]byte, error) {
 	parts := strings.Split(string(command), " ")
 	cmd := exec.Command(parts[0], url)
-
-	log.Println("start command:")
-	log.Printf("%+v", cmd)
-
 	return cmd.Output()
 }
