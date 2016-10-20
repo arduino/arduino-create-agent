48b4b354e47d7fb2bfc17f9da3c3e160ff4f2545
Matteo Suppo <matteo.suppo@gmail.com>
2016-10-20 09:12:38 +0200

    Add functions to reset the port

--- programmer/programmer.go
+++ programmer/programmer.go
@@ -1,8 +1,27 @@
 package programmer
 
+import (
+	"time"
+
+	"github.com/facchinm/go-serial"
+	"github.com/pkg/errors"
+)
+
 type logger interface {
-	Info()
-	Debug()
+	Debug(args ...interface{})
+	Info(args ...interface{})
+}
+
+func debug(l logger, args ...interface{}) {
+	if l != nil {
+		l.Debug(args...)
+	}
+}
+
+func info(l logger, args ...interface{}) {
+	if l != nil {
+		l.Info(args...)
+	}
 }
 
 // Auth contains username and password used for a network upload
@@ -24,5 +43,109 @@ type Extra struct {
 
 // Do performs a command on a port with a board attached to it
 func Do(port, board, path, commandline string, extra Extra, l logger) {
+	debug(l, port, board, path, commandline)
+	if extra.Network {
+		doNetwork()
+	} else {
+		doSerial(port, extra, l)
+	}
+}
 
+func doNetwork() {}
+
+func doSerial(port string, extra Extra, l logger) {
+	// some boards needs to be resetted
+	if extra.Use1200bpsTouch {
+		port, _ = reset(port, extra.WaitForUploadPort, l)
+	}
+}
+
+// reset opens the port at 1200bps. It returns the new port name (which could change
+// sometimes) and an error (usually because the port listing failed)
+func reset(port string, wait bool, l logger) (string, error) {
+	info(l, "Restarting in bootloader mode")
+
+	// Get port list before reset
+	ports, err := serial.GetPortsList()
+	debug(l, "Get port list before reset")
+	debug(l, ports, err)
+	if err != nil {
+		return "", errors.Wrapf(err, "Get port list before reset")
+	}
+
+	// Open port
+	mode := &serial.Mode{
+		BaudRate: 1200,
+		Vmin:     0,
+		Vtimeout: 1,
+	}
+	p, err := serial.OpenPort(port, mode)
+	debug(l, "Open port", port)
+	debug(l, p, err)
+	if err != nil {
+		return "", errors.Wrapf(err, "Open port %s", port)
+	}
+
+	// Set DTR
+	err = p.SetDTR(false)
+	debug(l, "Set DTR")
+	debug(l, err)
+	p.Close()
+
+	// Wait for port to disappear and reappear
+	if wait {
+		port = waitReset(ports, l)
+	}
+
+	return port, nil
+}
+
+// waitReset is meant to be called just after a reset. It watches the ports connected
+// to the machine until a port disappears and reappears. The port name could be different
+// so it returns the name of the new port.
+func waitReset(beforeReset []string, l logger) string {
+	var port string
+	timeout := false
+
+	go func() {
+		time.Sleep(10 * time.Second)
+		timeout = true
+	}()
+
+	// Wait for the port to disappear
+	debug(l, "Wait for the port to disappear")
+	for {
+		ports, err := serial.GetPortsList()
+		port = differ(ports, beforeReset)
+		debug(l, "..", ports, beforeReset, err, port)
+
+		if port != "" {
+			break
+		}
+		if timeout {
+			debug(l, ports, err, port)
+			break
+		}
+		time.Sleep(time.Millisecond * 100)
+	}
+
+	// Wait for the port to reappear
+	debug(l, "Wait for the port to reappear")
+	afterReset, _ := serial.GetPortsList()
+	for {
+		ports, err := serial.GetPortsList()
+		port = differ(ports, afterReset)
+		debug(l, "..", ports, afterReset, err, port)
+		if port != "" {
+			time.Sleep(time.Millisecond * 500)
+			break
+		}
+		if timeout {
+			debug(l, "timeout")
+			break
+		}
+		time.Sleep(time.Millisecond * 100)
+	}
+
+	return port
 }
---
+++ programmer/utils.go
@@ -0,0 +1,21 @@
+package programmer
+
+// differ returns the first item that differ between the two input slices
+func differ(slice1 []string, slice2 []string) string {
+	m := map[string]int{}
+
+	for _, s1Val := range slice1 {
+		m[s1Val] = 1
+	}
+	for _, s2Val := range slice2 {
+		m[s2Val] = m[s2Val] + 1
+	}
+
+	for mKey, mVal := range m {
+		if mVal == 1 {
+			return mKey
+		}
+	}
+
+	return ""
+}
