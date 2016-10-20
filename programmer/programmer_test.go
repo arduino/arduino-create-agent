3687da7d328c439d4f063c4cac9ef7e394ce92f5
Matteo Suppo <matteo.suppo@gmail.com>
2016-10-20 10:05:42 +0200

    Refactor, resolve is now public

--- programmer/programmer_test.go
+++ programmer/programmer_test.go
@@ -5,15 +5,12 @@ import (
 
 	"github.com/Sirupsen/logrus"
 	"github.com/arduino/arduino-create-agent/programmer"
-	"github.com/stretchr/testify/suite"
 )
 
-type ProgrammerTestSuite struct {
-	suite.Suite
-}
+type mockTools struct{}
 
-func TestProgrammer(t *testing.T) {
-	suite.Run(t, new(ProgrammerTestSuite))
+func (mockTools) GetLocation(el string) (string, error) {
+	return "$loc" + el, nil
 }
 
 var TestSerialData = []struct {
@@ -28,12 +25,35 @@ var TestSerialData = []struct {
 		`"{runtime.tools.avrdude.path}/bin/avrdude" "-C{runtime.tools.avrdude.path}/etc/avrdude.conf" {upload.verbose} {upload.verify} -patmega32u4 -cavr109 -P{serial.port} -b57600 -D "-Uflash:w:{build.path}/{build.project_name}.hex:i"`, programmer.Extra{Use1200bpsTouch: true, WaitForUploadPort: true}},
 }
 
-func (t *ProgrammerTestSuite) TestSerial() {
+func TestSerial(t *testing.T) {
 	logger := logrus.New()
 	logger.Level = logrus.DebugLevel
 
 	for _, test := range TestSerialData {
-		programmer.Do(test.Port, test.Board, test.File, test.Commandline, test.Extra, logger)
+		programmer.Do(test.Port, test.Board, test.File, test.Commandline, test.Extra, mockTools{}, logger)
 	}
-	t.Fail("")
+	t.Fail()
+}
+
+var TestResolveData = []struct {
+	Port        string
+	Board       string
+	File        string
+	Commandline string
+	Extra       programmer.Extra
+	Result      string
+}{
+	{"/dev/ttyACM0", "arduino:avr:leonardo", "./programmer_test.hex",
+		`"{runtime.tools.avrdude.path}/bin/avrdude" "-C{runtime.tools.avrdude.path}/etc/avrdude.conf" {upload.verbose} {upload.verify} -patmega32u4 -cavr109 -P{serial.port} -b57600 -D "-Uflash:w:{build.path}/{build.project_name}.hex:i"`, programmer.Extra{Use1200bpsTouch: true, WaitForUploadPort: true},
+		`"$loc$loc{runtime.tools.avrdude.path}/bin/avrdude" "-C{runtime.tools.avrdude.path}/etc/avrdude.conf"  $loc{upload.verify} -patmega32u4 -cavr109 -P/dev/ttyACM0 -b57600 -D "-Uflash:w:./programmer_test.hex:i"`},
+}
+
+func TestResolve(t *testing.T) {
+	for _, test := range TestResolveData {
+		result, _ := programmer.Resolve(test.Port, test.Board, test.File, test.Commandline, test.Extra, mockTools{})
+		if result != test.Result {
+			t.Error("expected " + test.Result + ", got " + result)
+			continue
+		}
+	}
 }
