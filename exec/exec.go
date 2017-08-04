87bbd1b8900e73102a6f35554c5f69bce596c190
Matteo Suppo <matteo.suppo@gmail.com>
2017-08-04 15:18:46 +0200

    Add package exec

---
+++ exec/exec.go
@@ -0,0 +1,106 @@
+/*
+ * This file is part of arduino-create-agent.
+ *
+ * arduino-create-agent is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, you may use this file as part of a free software
+ * library without restriction.  Specifically, if other files instantiate
+ * templates or use macros or inline functions from this file, or you compile
+ * this file and link it with other files to produce an executable, this
+ * file does not by itself cause the resulting executable to be covered by
+ * the GNU General Public License.  This exception does not however
+ * invalidate any other reasons why the executable file might be covered by
+ * the GNU General Public License.
+ *
+ * Copyright 2017 BCMI LABS SA (http://www.arduino.cc/)
+ */
+// Package exec is just syntactic sugar over os/exec. Allows to execute predefined
+// parametric commands in a safe way
+//
+// Usage:
+// 	cmd := exec.Command{
+// 		Pattern: "echo {interpolate.string}",
+// 		Params:  []string{"interpolate.string"},
+// 	}
+// 	opts := map[string]string{"interpolate.string": "hello world"}
+// 	stdout, stderr, err := exec.Local(cmd, opts)
+// 	fmt.Println(err) // nil
+// 	out, _ := ioutil.ReadAll(stdout)
+// 	stdout.Close()
+// 	stderr.Close()
+// 	fmt.Println(string(out)) // hello world
+package exec
+
+import (
+	"io"
+	"os/exec"
+	"strings"
+
+	shellwords "github.com/mattn/go-shellwords"
+	"github.com/pkg/errors"
+)
+
+// Command is a parametric command that can executed on the machine
+type Command struct {
+	// Pattern is the commandline that will be safely interpolated with the parameters
+	Pattern string
+	// Params contains a list of parameters that can be safely interpolated in the pattern
+	Params []string
+}
+
+// Interpolate substitutes the params with the corresponding options
+func (cmd Command) Interpolate(opts map[string]string) (interpolated []string, err error) {
+	pattern := cmd.Pattern
+	for key, value := range opts {
+		pattern = strings.Replace(pattern, "{"+key+"}", value, -1)
+	}
+
+	z, err := shellwords.Parse(pattern)
+	if err != nil {
+		return nil, err
+	}
+	return z, nil
+}
+
+// Local executes a command on the local machine, interpolating the command with the
+// given options
+func Local(c Command, opts map[string]string) (stdout, stderr io.ReadCloser, err error) {
+	// interpolate
+	inter, err := c.Interpolate(opts)
+	if err != nil {
+		return nil, nil, errors.Wrapf(err, "interpolate")
+	}
+
+	// create command
+	executable, args := inter[0], inter[1:]
+	cmd := exec.Command(executable, args...)
+
+	stdout, err = cmd.StdoutPipe()
+	if err != nil {
+		return nil, nil, errors.Wrapf(err, "retrieve output")
+	}
+
+	stderr, err = cmd.StderrPipe()
+	if err != nil {
+		return nil, nil, errors.Wrapf(err, "retrieve output")
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return stdout, stderr, nil
+}
---
+++ exec/exec_test.go
@@ -0,0 +1,183 @@
+/*
+ * This file is part of arduino-create-agent.
+ *
+ * arduino-create-agent is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, you may use this file as part of a free software
+ * library without restriction.  Specifically, if other files instantiate
+ * templates or use macros or inline functions from this file, or you compile
+ * this file and link it with other files to produce an executable, this
+ * file does not by itself cause the resulting executable to be covered by
+ * the GNU General Public License.  This exception does not however
+ * invalidate any other reasons why the executable file might be covered by
+ * the GNU General Public License.
+ *
+ * Copyright 2017 BCMI LABS SA (http://www.arduino.cc/)
+ */
+package exec_test
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"testing"
+
+	"github.com/arduino/arduino-create-agent/exec"
+)
+
+func Example() {
+	cmd := exec.Command{
+		Pattern: "echo {interpolate.string}",
+		Params:  []string{"interpolate.string"},
+	}
+	opts := map[string]string{"interpolate.string": "hello world"}
+	stdout, stderr, err := exec.Local(cmd, opts)
+	fmt.Println(err) // nil
+	out, _ := ioutil.ReadAll(stdout)
+	stdout.Close()
+	stderr.Close()
+	fmt.Println(string(out)) // hello world
+	// Output:
+	// <nil>
+	// hello world
+}
+
+func TestLocal(t *testing.T) {
+	cases := []struct {
+		ID        string
+		Pattern   string
+		Params    []string
+		Options   map[string]string
+		ExpStdout string
+		ExpStderr string
+		ExpErr    error
+	}{
+		{
+			"command not found",
+			"foo",
+			nil,
+			nil,
+			"",
+			"",
+			errors.New(`exec: "foo": executable file not found in $PATH`),
+		},
+		{
+			"error",
+			"foo '",
+			nil,
+			nil,
+			"",
+			"",
+			errors.New("interpolate: invalid command line string"),
+		},
+		{
+			"hello world",
+			"echo {interpolate.string}",
+			[]string{"interpolate.string"},
+			map[string]string{
+				"interpolate.string": "hello world",
+				"ignored":            "ignored",
+			},
+			"hello world",
+			"",
+			nil,
+		},
+		{
+			"inject ; to perform other commands has no effect",
+			"echo {interpolate.string}",
+			[]string{"interpolate.string"},
+			map[string]string{
+				"interpolate.string": "hello world; ls",
+				"ignored":            "ignored",
+			},
+			"hello world",
+			"",
+			nil,
+		},
+		{
+			"bash expansion do not work",
+			"echo {interpolate.string}",
+			[]string{"interpolate.string"},
+			map[string]string{
+				"interpolate.string": "`date`",
+				"ignored":            "ignored",
+			},
+			"`date`",
+			"",
+			nil,
+		},
+		{
+			"env vars do not work",
+			"echo {interpolate.string}",
+			[]string{"interpolate.string"},
+			map[string]string{
+				"interpolate.string": "$USER",
+				"ignored":            "ignored",
+			},
+			"$USER",
+			"",
+			nil,
+		},
+		{
+			"return stderr",
+			"ls /notexist",
+			[]string{"interpolate.string"},
+			map[string]string{
+				"interpolate.string": "$USER",
+				"ignored":            "ignored",
+			},
+			"",
+			"ls: cannot access '/notexist': No such file or directory",
+			nil,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("%s", tc.ID), func(t *testing.T) {
+			command := exec.Command{
+				Pattern: tc.Pattern,
+				Params:  tc.Params,
+			}
+			stdout, stderr, e := exec.Local(command, tc.Options)
+			if !errEq(e, tc.ExpErr) {
+				t.Errorf("expected e to be '%s', was '%s'", tc.ExpErr, e)
+				return
+			}
+			if e != nil {
+				return
+			}
+			if stdout == nil || stderr == nil {
+				t.Errorf("stdout and stderr should not be nil")
+				return
+			}
+			out, _ := ioutil.ReadAll(stdout)
+			stdout.Close()
+
+			if strings.TrimSpace(string(out)) != tc.ExpStdout {
+				t.Errorf("expected stdout to be '%s', was '%s'", tc.ExpStdout, out)
+			}
+			err, _ := ioutil.ReadAll(stderr)
+			stderr.Close()
+			if strings.TrimSpace(string(err)) != tc.ExpStderr {
+				t.Errorf("expected stderr to be '%s', was '%s'", tc.ExpStderr, err)
+			}
+		})
+	}
+}
+
+func errEq(err1, err2 error) bool {
+	return fmt.Sprintf("%s", err1) == fmt.Sprintf("%s", err2)
+}
