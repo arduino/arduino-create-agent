fcf6d82ee901865be667746ec4cfd13717189391
umbynos <umberto.baldi@edu.unito.it>
2021-01-22 17:59:13 +0100

    Run e2e tests only with GUI version on macos (Github runners are basically mac mini with GUI support)"

--- test/conftest.py
+++ test/conftest.py
@@ -12,7 +12,7 @@ from invoke.context import Context
 @pytest.fixture(scope="function")
 def agent(pytestconfig):
     
-    agent_cli = str(Path(pytestconfig.rootdir) / "arduino-create-agent_cli")
+    agent_cli = str(Path(pytestconfig.rootdir) / "arduino-create-agent")
     env = {
         # "ARDUINO_DATA_DIR": data_dir,
         # "ARDUINO_DOWNLOADS_DIR": downloads_dir,
@@ -24,7 +24,7 @@ def agent(pytestconfig):
     
     cd_command = "cd"
     with run_context.prefix(f'{cd_command} ..'):
-        runner.run(command=agent_cli, echo=True, hide=True, warn=True, env=env, asynchronous=True)
+        runner.run(agent_cli, echo=True, hide=True, warn=True, env=env, asynchronous=True)
         
         # we give some time to the agent to start and listen to
         # incoming requests
