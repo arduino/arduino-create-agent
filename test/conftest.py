b1579eeba9ecb04114ad1701f9e5e4a61be7f031
Maurizio Branca <m.branca@arduino.cc>
2021-02-03 11:54:23 +0000

    [EDITOR-527] E2E testing infrastructure (#591)
    * Bootstrap E2E testing infrastructure
    
    * add e2e tests in taskfile and in CI
    
    * fix deprecation warning when running pytest
    
    * Run e2e tests only with GUI version on macos (Github runners are basically mac mini with GUI support)"
    
    * add first draft of tests for certs and v2 APIs
    
    * add logs to .gitignore
    
    * skip update, currently no way of testing this on gh runners
    
    Co-authored-by: umbynos <umberto.baldi@edu.unito.it>

---
+++ test/conftest.py
@@ -0,0 +1,45 @@
+import os
+import platform
+import signal
+import time
+from pathlib import Path
+
+import pytest
+from invoke import Local
+from invoke.context import Context
+
+
+@pytest.fixture(scope="function")
+def agent(pytestconfig):
+    
+    agent_cli = str(Path(pytestconfig.rootdir) / "arduino-create-agent")
+    env = {
+        # "ARDUINO_DATA_DIR": data_dir,
+        # "ARDUINO_DOWNLOADS_DIR": downloads_dir,
+        # "ARDUINO_SKETCHBOOK_DIR": data_dir,
+    }
+    run_context = Context()
+
+    runner = Local(run_context) # execute a command on the local filesystem
+    
+    cd_command = "cd"
+    with run_context.prefix(f'{cd_command} ..'):
+        runner.run(agent_cli, echo=True, hide=True, warn=True, env=env, asynchronous=True)
+        
+        # we give some time to the agent to start and listen to
+        # incoming requests
+        time.sleep(.5)
+
+        # we block here until the test function using this fixture has returned
+        yield runner
+
+    # Kill the runner's process as we finished our test (platform dependent)
+    os_signal = signal.SIGTERM
+    if platform.system() != "Windows":
+        os_signal = signal.SIGKILL
+    os.kill(runner.process.pid, os_signal)
+
+
+@pytest.fixture(scope="session")
+def base_url():
+    return "http://127.0.0.1:8991"
---
+++ test/test_update.py
@@ -0,0 +1,33 @@
+# import json
+import psutil
+import requests
+import pytest
+
+# test if the update process succeeds in terminating the binary
+@pytest.mark.skip(reason="no way of currently testing this")
+def test_update_shutdown(base_url, agent):
+
+    procs=[]
+    for p in psutil.process_iter():
+        if p.name() == "arduino-create-agent":
+            procs.append(p)
+
+    resp = requests.post(f"{base_url}/update")
+    # assert resp.status_code == 200
+    # assert "Please wait a moment while the agent reboots itself" in info['success'] # failing on macos see https://github.com/arduino/arduino-create-agent/issues/608
+    gone, alive = psutil.wait_procs(procs, timeout=3, callback=on_terminate) # wait for "arduino-create-agent" to terminate
+
+def on_terminate(proc):
+    print("process {} terminated with exit code {}".format(proc, proc.returncode))
+    assert True
+
+# the version currently running is the latest available
+@pytest.mark.skip(reason="no way of currently testing this")
+def test_latest_version(base_url, agent):
+    resp = requests.get(f"{base_url}/info")
+    assert resp.status_code == 200
+    latest_version = requests.get("https://s3.amazonaws.com/arduino-create-static/agent-metadata/agent-version.json") # get the latest version available
+
+    version = latest_version.json()
+    info = resp.json()
+    assert info["version"] == version["Version"]
