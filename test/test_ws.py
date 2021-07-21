fc1222ef930cd37d5eb9683b1807c6c7a87c0df6
umbynos <umberto.baldi@edu.unito.it>
2021-07-21 11:24:23 +0200

    add callback to capture returned message, add new test for serial

--- test/test_ws.py
+++ test/test_ws.py
@@ -1,5 +1,4 @@
-import asyncio
-import socketio as io
+import socketio
 import time
 
 def test_ws_connection(agent, socketio):
@@ -7,5 +6,19 @@ def test_ws_connection(agent, socketio):
     assert socketio.sid is not None
 
 def test_list(agent, socketio):
+    socketio.on('message', message_handler)
     socketio.emit('command', 'list')
-    
+    time.sleep(.1)
+
+def test__open_serial_default(agent, socketio):
+    socketio.on('message', message_handler)
+    socketio.emit('command', 'open /dev/ttyACM0 9600')
+    time.sleep(.1)
+    socketio.emit('command', 'send /dev/ttyACM0 /"ciao/"')
+    time.sleep(.1)
+    socketio.emit('command', 'close /dev/ttyACM0')
+    time.sleep(.1)
+
+
+def message_handler(msg):
+    print('Received message: ', msg)
