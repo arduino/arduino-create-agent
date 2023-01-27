f557b299abf39e77637bfd27a0d9b186f877b371
Cristian Maglie <c.maglie@bug.st>
2023-01-27 18:10:21 +0100

    Implemented the autoupdater for MacOS

--- updater/updater_darwin.go
+++ updater/updater_darwin.go
@@ -15,10 +15,105 @@
 
 package updater
 
+import (
+	"bytes"
+	"context"
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/arduino/go-paths-helper"
+	"github.com/codeclysm/extract/v3"
+)
+
 func start(src string) string {
 	return ""
 }
 
 func checkForUpdates(currentVersion string, updateAPIURL, updateBinURL string, cmdName string) (string, error) {
-	return "", nil
+	executablePath, err := os.Executable()
+	if err != nil {
+		return "", fmt.Errorf("could not app path: %w", err)
+	}
+	currentAppPath := paths.New(executablePath).Parent().Parent().Parent()
+	if currentAppPath.Ext() != ".app" {
+		return "", fmt.Errorf("could not find app root in %s", executablePath)
+	}
+	oldAppPath := currentAppPath.Parent().Join("ArdiunoCreateAgent.old.app")
+	if oldAppPath.Exist() {
+		return "", fmt.Errorf("temp app already exists: %s, cannot update", oldAppPath)
+	}
+
+	// Fetch information about updates
+	info, err := fetchInfo(updateAPIURL, cmdName)
+	if err != nil {
+		return "", err
+	}
+	if info.Version == currentVersion {
+		// No updates available, bye bye
+		return "", nil
+	}
+
+	tmp := paths.TempDir().Join("arduino-create-agent")
+	tmpZip := tmp.Join("update.zip")
+	tmpAppPath := tmp.Join("ArduinoCreateAgent-update.app")
+	defer tmp.RemoveAll()
+
+	// Download the update.
+	download, err := fetch(updateBinURL + cmdName + "/" + plat + "/" + info.Version + "/" + cmdName)
+	if err != nil {
+		return "", err
+	}
+	defer download.Close()
+
+	f, err := tmpZip.Create()
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	sha := sha256.New()
+	if _, err := io.Copy(io.MultiWriter(sha, f), download); err != nil {
+		return "", err
+	}
+	f.Close()
+
+	// Check the hash
+	if s := sha.Sum(nil); !bytes.Equal(s, info.Sha256) {
+		return "", fmt.Errorf("bad hash: %s (expected %s)", s, info.Sha256)
+	}
+
+	// Unzip the update
+	if err := tmpAppPath.MkdirAll(); err != nil {
+		return "", fmt.Errorf("could not create tmp dir to unzip update: %w", err)
+	}
+
+	f, err = tmpZip.Open()
+	if err != nil {
+		return "", fmt.Errorf("could not open archive for unzip: %w", err)
+	}
+	defer f.Close()
+	if err := extract.Archive(context.Background(), f, tmpAppPath.String(), nil); err != nil {
+		return "", fmt.Errorf("extracting archive: %w", err)
+	}
+
+	// Rename current app as .old
+	if err := currentAppPath.Rename(oldAppPath); err != nil {
+		return "", fmt.Errorf("could not rename old app as .old: %w", err)
+	}
+
+	// Install new app
+	if err := tmpAppPath.CopyDirTo(currentAppPath); err != nil {
+		// Try rollback changes
+		_ = currentAppPath.RemoveAll()
+		_ = oldAppPath.Rename(currentAppPath)
+		return "", fmt.Errorf("could not install app: %w", err)
+	}
+
+	// Remove old app
+	_ = oldAppPath.RemoveAll()
+
+	// Restart agent
+	return currentAppPath.Join("Contents", "MacOS", "Arduino_Create_Agent").String(), nil
 }
