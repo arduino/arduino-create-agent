b6f6947d61cabe12df934b7842ef16ef39f7b1b7
Umberto Baldi <u.baldi@arduino.cc>
2023-04-19 14:52:04 +0200

    Revert "add cert install on win"
    This reverts commit bcaeb75adcc1c2e0a0e9df916fca0740d93da17e.

--- certificates/install_default.go
+++ certificates/install_default.go
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-//go:build !darwin && !windows
+//go:build !darwin
 
 package certificates
 
