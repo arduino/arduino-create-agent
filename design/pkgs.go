09c82f731bcf167e775a89a1f6234367f73e0316
Umberto Baldi <u.baldi@arduino.cc>
2023-09-06 16:52:40 +0200

    add signature

--- design/pkgs.go
+++ design/pkgs.go
@@ -129,6 +129,11 @@ var ToolPayload = Type("arduino.tool", func() {
 		Example("SHA-256:1ae54999c1f97234a5c603eb99ad39313b11746a4ca517269a9285afa05f9100")
 	})
 
+	Attribute("signature", String, `The signature used to sign the url. Mandatory when url is present.
+	This ensure the security of the file downloaded`, func() {
+		Example("382898a97b5a86edd74208f10107d2fecbf7059ffe9cc856e045266fb4db4e98802728a0859cfdcda1c0b9075ec01e42dbea1f430b813530d5a6ae1766dfbba64c3e689b59758062dc2ab2e32b2a3491dc2b9a80b9cda4ae514fbe0ec5af210111b6896976053ab76bac55bcecfcececa68adfa3299e3cde6b7f117b3552a7d80ca419374bb497e3c3f12b640cf5b20875416b45e662fc6150b99b178f8e41d6982b4c0a255925ea39773683f9aa9201dc5768b6fc857c87ff602b6a93452a541b8ec10ca07f166e61a9e9d91f0a6090bd2038ed4427af6251039fb9fe8eb62ec30d7b0f3df38bc9de7204dec478fb86f8eb3f71543710790ee169dce039d3e0")
+	})
+
 	Required("name", "version", "packager")
 })
 
