3544cc1d18b15c6f1bd830f40f90791a3a6d3df0
Umberto Baldi <u.baldi@arduino.cc>
2023-10-27 10:48:36 +0200

    remove dark mode detection (it was not working)

---
+++ icon/icon_darwin.go
@@ -0,0 +1,23 @@
+package icon
+
+import _ "embed" // import embed to embed the icon
+
+// GetIcon will return the icon
+func GetIcon() []byte {
+	return data
+}
+
+// GetIconHiber will return the hibernated icon
+func GetIconHiber() []byte {
+	return dataHibernate
+}
+
+// data represents the icon
+//
+//go:embed icon_mac.png
+var data []byte
+
+// dataHibernate represents the icon hibernated
+//
+//go:embed icon_mac_hiber.png
+var dataHibernate []byte
