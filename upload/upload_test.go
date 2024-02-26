13c9b5d0de739b0eebecf9c248e2822129910e8e
Umberto Baldi <u.baldi@arduino.cc>
2024-02-26 12:34:29 +0100

    Put upload test under upload package

--- upload/upload_test.go
+++ upload/upload_test.go
@@ -13,14 +13,13 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-package upload_test
+package upload
 
 import (
 	"log"
 	"strings"
 	"testing"
 
-	"github.com/arduino/arduino-create-agent/upload"
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/sirupsen/logrus"
 )
@@ -36,11 +35,11 @@ var TestSerialData = []struct {
 	Name        string
 	Port        string
 	Commandline string
-	Extra       upload.Extra
+	Extra       Extra
 }{
 	{
 		"leonardo", "/dev/ttyACM0",
-		`"$HOME/.arduino-create/avrdude/6.3.0-arduino6/bin/avrdude" "-C$HOME/.arduino-create/avrdude/6.3.0-arduino6/etc/avrdude.conf" -v -patmega32u4 -cavr109 -P/dev/ttyACM0 -b57600 -D "-Uflash:w:./upload_test.hex:i"`, upload.Extra{Use1200bpsTouch: true, WaitForUploadPort: true}},
+		`"$HOME/.arduino-create/avrdude/6.3.0-arduino6/bin/avrdude" "-C$HOME/.arduino-create/avrdude/6.3.0-arduino6/etc/avrdude.conf" -v -patmega32u4 -cavr109 -P/dev/ttyACM0 -b57600 -D "-Uflash:w:./upload_test.hex:i"`, Extra{Use1200bpsTouch: true, WaitForUploadPort: true}},
 }
 
 func TestSerial(t *testing.T) {
@@ -51,7 +50,7 @@ func TestSerial(t *testing.T) {
 
 	for _, test := range TestSerialData {
 		commandline := strings.Replace(test.Commandline, "$HOME", home, -1)
-		err := upload.Serial(test.Port, commandline, test.Extra, logger)
+		err := Serial(test.Port, commandline, test.Extra, logger)
 		log.Println(err)
 	}
 }
@@ -61,12 +60,16 @@ var TestResolveData = []struct {
 	File         string
 	PlatformPath string
 	Commandline  string
-	Extra        upload.Extra
+	Extra        Extra
 	Result       string
 }{
-	{"arduino:avr:leonardo", "./upload_test.hex", "",
-		`"{runtime.tools.avrdude.path}/bin/avrdude" "-C{runtime.tools.avrdude.path}/etc/avrdude.conf" -v {upload.verify} -patmega32u4 -cavr109 -P/dev/ttyACM0 -b57600 -D "-Uflash:w:{build.path}/{build.project_name}.hex:i"`, upload.Extra{Use1200bpsTouch: true, WaitForUploadPort: true},
-		`"$loc$loc{runtime.tools.avrdude.path}/bin/avrdude" "-C{runtime.tools.avrdude.path}/etc/avrdude.conf" -v $loc{upload.verify} -patmega32u4 -cavr109 -P/dev/ttyACM0 -b57600 -D "-Uflash:w:./upload_test.hex:i"`},
+	{"arduino:avr:leonardo",
+		"./upload_test.hex",
+		"",
+		`{runtime.tools.avrdude.path}/bin/avrdude -C{runtime.tools.avrdude.path}/etc/avrdude.conf -v {upload.verify} -patmega32u4 -cavr109 -P/dev/ttyACM0 -b57600 -D "-Uflash:w:{build.path}/{build.project_name}.hex:i"`,
+		Extra{Use1200bpsTouch: true, WaitForUploadPort: true},
+		`$loc$loc{runtime.tools.avrdude.path}/bin/avrdude -C{runtime.tools.avrdude.path}/etc/avrdude.conf -v $loc{upload.verify} -patmega32u4 -cavr109 -P/dev/ttyACM0 -b57600 -D "-Uflash:w:./upload_test.hex:i"`,
+	},
 }
 
 func TestResolve(t *testing.T) {
